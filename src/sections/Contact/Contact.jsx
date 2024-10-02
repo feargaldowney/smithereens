237c5093adec1001a6c0055a7225174429c15c4a
Feargal <Feargaldowney123@gmail.com>
2024-10-02 13:54:08 +0100

    Reset form on submit

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,40 @@
 import styles from './Contact.module.css'
+import { useState } from 'react';
 
 function Contact() {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const formData = new FormData(form);
+
+        try {
+            const response = await fetch("https://formspree.io/f/xjkbnqrd", {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Accept:' 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                form.reset();
+                setIsSubmitted(true);
+            } else {
+                console.error('Form submission failed');
+            }
+        } catch (error) {
+            console.error('Error submitting this form:', error);
+        }
+    };
+
   return (
     <section id='contact' className={styles.container}>
         <h1 className='sectionTitle'>Contact</h1>
-        <form action="https://formspree.io/f/xjkbnqrd" method="post">
+        <form onSubmit={handleSubmit}>
             <div className='formGroup'>
                 <label htmlFor="name" hidden>Name</label>
                 <input 
@@ -40,7 +70,6 @@ function Contact() {
                 value="Submit"
             />
         </form>
-
     </section>
   )
 }
