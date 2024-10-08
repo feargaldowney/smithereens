715ca051b7d536de46795c8d40aa521ddea66334
Feargal <Feargaldowney123@gmail.com>
2024-10-08 14:13:03 +0100

    added new background

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,30 +1,16 @@
 import styles from "./HeroStyles.module.css";
-import sun from "../../assets/sun.svg";
-import moon from "../../assets/moon.svg";
 import instagram from '../../assets/instagram.png';
 import spotify from '../../assets/spotify.png';
 
-import { useTheme } from "../../common/ThemeContext";
-
 function Hero() {
-  const {theme, toggleTheme} = useTheme();
-  const themeIcon = theme === 'light' ? sun : moon;
   return <section id="hero" className={styles.container}>
         <div className="{styles.info}">
-          <div className={styles.colorModeContainer}>
-            <img 
-              className ={styles.colorMode}
-              src={themeIcon} 
-              alt="Color mode icon" 
-              onClick={toggleTheme}
-            />
-          </div>
             <h1>
                 Smithereens
             </h1>
             <h3>A classic thing to be blown into.</h3>
             <span>
-                <a href="https://www.instagram.com/pherg__/">
+                <a href="https://www.instagram.com/smithereeeeeeeens/">
                     <img src={instagram} alt="Instagram Icon"/>
                 </a>
                 <a href="https://open.spotify.com/artist/6xC6vekNkKWw5rP5yM9GJ6">
