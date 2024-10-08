7a5aeb558e7a2fcc0e3ccc952ede0837efabcf60
Feargal <Feargaldowney123@gmail.com>
2024-10-08 21:30:28 +0100

    fixed styling of embedded video on mobile view

--- src/sections/Youtube/YouTube.jsx
+++ src/sections/Youtube/YouTube.jsx
@@ -5,6 +5,7 @@ function YouTube() {
     <section id='youtube' className={styles.container}>
         <h2 className='sectionTitle'>Our videos</h2>
         <iframe className={styles.iframe} src="https://www.youtube.com/embed/4bTrGqf2wH4?si=X_6byinsMpBv4x0v" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen/>
+        
     </section>
     );
     };
