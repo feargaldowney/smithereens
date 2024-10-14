e9fc0e9f92fe47be34802ec6ead336f8bcba1e97
Feargal <Feargaldowney123@gmail.com>
2024-10-14 12:48:49 +0100

    minor UI fix

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -41,7 +41,7 @@ function Contact() {
                 type="text" 
                 name="name" 
                 id="name" 
-                placeholder='name'
+                placeholder='Name'
                 required 
                 />
             </div>            
